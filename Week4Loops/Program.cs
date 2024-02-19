2eb9b0f7e71eccf404670548e0bbc20652957bd9
Maire Roosma. <roosmamaire@gmail.com>
2024-02-19 20:00:20 +0200

    add week4Loops code

---
+++ Week4Loops/Program.cs
@@ -0,0 +1,13 @@
+﻿//FOR - LOOP
+//F DRY principle- Do not repeat yourself
+int i = 0;
+for(i = 0; i < 5; i++) //i++ = i+1
+{
+    Console.WriteLine("We don´t need no education");
+}
+Console.WriteLine("Have a nice day");
+//Console.WriteLine("We don´t need no education");
+//Console.WriteLine("We don´t need no education");
+//Console.WriteLine("We don´t need no education");
+//Console.WriteLine("We don´t need no education");
+//Console.WriteLine("We don´t need no education");
---
+++ WhileLoops/Program.cs
@@ -0,0 +1,25 @@
+﻿
+Random rnd = new Random();
+int cpuRandom;
+
+bool loopActice = true;// bool - True/false
+
+while(loopActice)
+{
+    cpuRandom = rnd.Next(1, 4);
+   // Console.WriteLine($"cpu has generated {cpuRandom}");
+    Console.WriteLine("Make a  guess, enter a  number 1-3");
+    int userNumber = Int32.Parse(Console.ReadLine());
+
+    if(userNumber == cpuRandom)
+    {
+        Console.WriteLine("Congratulations, you won!");
+        // loopActice = false;- toimub sama moodi nagu break;
+        break;
+    }
+    else
+    {
+        Console.WriteLine("Oops. Try again");
+    }
+}
+Console.WriteLine("Have a nice day");
